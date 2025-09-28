f091f86c469e0efbb3239971194093d504560337
Geusan31 <edurais.ariadaffa@gmail.com>
2025-09-28 13:23:57 +0700

    update profile

--- src/layout/PublicNavbar.tsx
+++ src/layout/PublicNavbar.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link';
 import React from 'react';
 import Image from 'next/image';
+import { useAuth } from './AuthContext';
+import ProfileDropdown from '@/components/header/ProfileDropdown';
 
 export default function PublicNavbar() {
+  const user = useAuth();
   return (
     <header className="sticky top-0 z-50 w-full bg-white/80  backdrop-blur-sm border-b border-gray-200 dark:border-gray-700">
       <nav className="container mx-auto flex justify-between items-center px-4 py-4 sm:px-6 lg:px-24">
@@ -13,41 +16,43 @@ export default function PublicNavbar() {
               Edupath
             </a>
           </Link>
-        <div className='flex items-center gap-x-6 px-8'>
-          <Link href="/"  legacyBehavior>
-          <a className="text-brand-400 hover:scale-105 hover:text-brand-500 trasnition duration-100">
-            Home
-          </a>
-          </Link>
-          <Link href=""  legacyBehavior>
-          <a className="text-brand-400 hover:scale-105 hover:text-brand-500 trasnition duration-100">
-            E-Learning
-          </a>
-          </Link>
-          <Link href=""  legacyBehavior>
-          <a className="text-brand-400 hover:scale-105 hover:text-brand-500 trasnition duration-100">
-            Info Loker
-          </a>
-          </Link>
-          <Link href=""  legacyBehavior>
-          <a className="text-brand-400 hover:scale-105 hover:text-brand-500 trasnition duration-100">
-            Komunitas
-          </a>
-          </Link>
+          <div className='flex items-center gap-x-6 px-8'>
+            <Link href="/" legacyBehavior>
+              <a className="text-brand-400 hover:scale-105 hover:text-brand-500 trasnition duration-100">
+                Home
+              </a>
+            </Link>
+            <Link href="" legacyBehavior>
+              <a className="text-brand-400 hover:scale-105 hover:text-brand-500 trasnition duration-100">
+                E-Learning
+              </a>
+            </Link>
+            <Link href="" legacyBehavior>
+              <a className="text-brand-400 hover:scale-105 hover:text-brand-500 trasnition duration-100">
+                Info Loker
+              </a>
+            </Link>
+            <Link href="" legacyBehavior>
+              <a className="text-brand-400 hover:scale-105 hover:text-brand-500 trasnition duration-100">
+                Komunitas
+              </a>
+            </Link>
+          </div>
         </div>
-        </div>
-        <div className="flex items-center gap-4">
-          <Link href="/auth/signin" legacyBehavior>
-            <a className="px-4 py-2 outline-2 outline-brand-500 hover:outline-brand-400 rounded-lg text-brand-500 hover:text-brand-400 hover:bg-brand-25 transition-colors">
-              Sign In
-            </a>
-          </Link>
-          <Link href="/auth/signup" legacyBehavior>
-            <a className="px-4 py-2 text-white bg-brand-500 rounded-lg shadow-md hover:bg-brand-400 transition-colors">
-              Sign Up
-            </a>
-          </Link>
-        </div>
+        {user.isAuthenticated ? (<ProfileDropdown />) : (
+          <div className="flex items-center gap-4">
+            <Link href="/auth/signin" legacyBehavior>
+              <a className="px-4 py-2 outline-2 outline-brand-500 hover:outline-brand-400 rounded-lg text-brand-500 hover:text-brand-400 hover:bg-brand-25 transition-colors">
+                Sign In
+              </a>
+            </Link>
+            <Link href="/auth/signup" legacyBehavior>
+              <a className="px-4 py-2 text-white bg-brand-500 rounded-lg shadow-md hover:bg-brand-400 transition-colors">
+                Sign Up
+              </a>
+            </Link>
+          </div>
+        )}
       </nav>
     </header>
   );
